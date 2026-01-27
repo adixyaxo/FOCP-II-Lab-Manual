2ad8e09b4f9e6487221a255a1970565a96b14066
Aditya <aditya0dagar@gmail.com>
2026-01-27 12:03:07 +0530

    Q5 part 2

--- Q5.cpp
+++ Q5.cpp
@@ -19,5 +19,20 @@ int main()
     cout << "var 1 is " << var1 << endl;
     cout << "var 2 is " << var2 << endl;
 
+    cout << endl;
+
+    // Using temp var
+    int int1, int2, temp;
+    cout << "Using temp variable" << endl;
+    cout << "Please enter two numbers :: ";
+    cin >> var1 >> var2;
+    cout << "var 1 is " << var1 << endl;
+    cout << "var 2 is " << var2 << endl;
+    temp = int1;
+    int1 = int2;
+    int2 = temp;
+    cout << "var 1 is " << int1 << endl;
+    cout << "var 2 is " << int2 << endl;
+
     return 0;
 }
