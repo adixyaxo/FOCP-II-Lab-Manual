f09fef0f1c91109db45434e38082715936d984fc
Aditya <aditya0dagar@gmail.com>
2026-02-01 15:08:27 +0530

    starting solving q13

---
+++ Q13.cpp
@@ -0,0 +1,12 @@
+// Q13. A mathematics tool computes the exact roots of a quadratic equation for teaching purposes. Implement a solution to calculate the roots of a quadratic equation.
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
