0186f12d72a37f9dbf6554a68861e93f4fe984de
Aditya <aditya0dagar@gmail.com>
2026-02-09 09:53:34 +0530

    forward indexing and backward indexing

--- functions.cpp
+++ functions.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-
-int sum(int a, int b)
+int diff(void);       // forward indexing
+int sum(int a, int b) // backward indexing
 {
     return a + b;
 }
@@ -9,6 +9,10 @@ int sum(int a, int b)
 int main()
 {
     cout << sum(10, 20);
-
     return 0;
+}
+
+int diff(int a, int b)
+{
+    return a - b;
 }
