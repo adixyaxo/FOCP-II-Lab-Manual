a11366b7aac0c697171424d3a1a84f65bc6d2861
Aditya <aditya0dagar@gmail.com>
2026-02-20 23:58:00 +0530

    learning pointers and refrence by trying

---
+++ pointers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int *pointer = &a;
+    int &refrence = a;
+    int &refrenceofrefrence = refrence;
+    int &refrenceofpointer = *pointer;
+    int *pointerofpointer = &*pointer;
+
+    cout << "a  ::  " << a << endl;                                 // value of a
+    cout << "&a  ::  " << &a << endl;                               // address of a
+    cout << "pointer  ::  " << pointer << endl;                     // &a ie address of a
+    cout << "*pointer  ::  " << *pointer << endl;                   // value at pointer ie value at address stored in pointer ie value at &a which is value of a
+    cout << "&pointer  ::  " << &pointer << endl;                   // address of the pointer variable itself
+    cout << "pointerofpointer  ::  " << pointerofpointer << endl;   // address of the variable stored in pointerofpointer ie *pointer so the value of pointerofpointer is address of *pointer ie address of value stored in pointer which is address of a
+    cout << "*pointerofpointer  ::  " << *pointerofpointer << endl; // value at address stored in pointerofpointer which is address of *pointer so value at address of a which is value of a
+    cout << "&pointerofpointer  ::  " << &pointerofpointer << endl;
+    cout << "refrence  ::  " << refrence << endl;
+    cout << "&refrence  ::  " << &refrence << endl;
+    cout << "refrenceofrefrence  ::  " << refrenceofrefrence << endl;
+    cout << "&refrenceofrefrence  ::  " << &refrenceofrefrence << endl;
+    cout << "refrenceofpointer  ::  " << refrenceofpointer << endl;
+    cout << "&refrenceofpointer  ::  " << &refrenceofpointer << endl;
+    return 0;
+}
