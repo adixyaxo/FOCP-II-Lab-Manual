e95a2b14788448d80c73b4095be51aa5373893a5
Aditya <aditya0dagar@gmail.com>
2026-03-02 21:22:25 +0530

    encapsulation

--- Encapsulation.cpp
+++ Encapsulation.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // What is encapsulation in C++?
-// Encapsulation is one of the fundamental principles of object-oriented programming (OOP) in C
+// Encapsulation is one of the fundamental principles of object-oriented programming (OOP) in C++. It refers to the bundling of data (attributes) and methods (functions) that operate on that data into a single unit called a class. Encapsulation allows for data hiding, which means that the internal representation of an object is hidden from the outside world, and access to that data is restricted through public member functions (getters and setters). This helps to protect the integrity of the data and prevents unauthorized access or modification.
 class student
 {
 private:
@@ -20,10 +20,10 @@ public:
 };
 
 int main()
-    student s1;
-s1.setMarks(95);
-cout << "Marks: " << s1.getMarks() << endl;
 {
+    student s1;
+    s1.setMarks(95);
+    cout << "Marks: " << s1.getMarks() << endl;
 
     return 0;
 }
