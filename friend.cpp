68f4b3cffcaece777eab38479cffe226800a5211
adixyaxo <aditya0dagar@gmail.com>
2026-05-10 13:26:57 +0530

    static

--- friend.cpp
+++ friend.cpp
@@ -21,6 +21,14 @@ public:
 
     };
     friend void area(rectangle obj);
+    /*
+    Friend functions in C++ are primarily used to grant non-member functions access to the private and protected members of a class.  This feature is essential for several specific scenarios:
+
+    Operator Overloading: They allow operators to access private data when the operation involves multiple objects or types that do not fit naturally into a member function signature.
+    Inter-Class Communication: They enable tight coupling between classes, allowing one class to share internal details with another without exposing them publicly.
+    Encapsulation Balance: They provide a controlled way to expose specific internal data to external functions, maintaining better encapsulation than making all data public while avoiding the verbosity of multiple getter and setter methods.
+    However, because friend functions break strict encapsulation and can increase coupling between classes, they should be used judiciously to avoid making code difficult to maintain or debug.
+    */
     void display();
 };
 
--- static.cpp
+++ static.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+static int x = 10; // here we are using static globally ie outside of a class thus here the use of static keyword is diffrent here
+// here the keyword static makes the integer x private for the file ie other cpp files wont be able to access my variable x
+// this is only going to be visible for this c++ file and invisible to the other c++ files
+// this is very important for privacy and coding and not having bugs with totally global varibales for all files which is pretty specifically bad
+
+// same way it works for the functions
+static void print(void){cout<<"This is a static function only working in this file"<<endl;}
+
 class box
 {
 private:
@@ -9,7 +17,8 @@ private:
     double h;
 
 public:
-    static int count;
+    static int count; // here the usage of static is inside the class hence the count has become a single variable which would be refered by all the objects of this class
+    // ie all the objects of this class are going to refer to the same cout ie the count of the class
     box(double length = 0, double width = 0, double height = 0)
     {
         l = length;
